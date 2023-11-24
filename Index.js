6f8d14cd7a06de26846d274d901dc02867bab674
Astridrdzza03 <108295938+Astridrdzza03@users.noreply.github.com>
2023-11-23 19:28:29 -0600

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,7 +1,19 @@
 function validar()
 {
-    var user = document.getElementById("user");
-    alert(user.value);
-    var pass = document.getElementById("password");
-    alert(pass.value);
+    var bandera= false;
+    if(bandera)
+    {
+        console.log("Validacion completa")
+        //Rediccione a las paginas correspondiente
+    }
+    //var user = document.getElementById("user");
+    //alert(user.value);
+    //var pass = document.getElementById("password");
+    //alert(pass.value);
+}
+
+function correctCaptcha()
+{
+    console.log("Captcha resuelto correctamente")
+    bandera= true;
 }
